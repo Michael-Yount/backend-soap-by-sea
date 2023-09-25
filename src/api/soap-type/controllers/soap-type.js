1ff28b7125d7c2c99101319dfb4c2e35684a046b
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-24 20:24:09 -0400

    fisrt commit

---
+++ src/api/soap-type/controllers/soap-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * soap-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::soap-type.soap-type');
---
+++ src/api/soap-type/services/soap-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * soap-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::soap-type.soap-type');
